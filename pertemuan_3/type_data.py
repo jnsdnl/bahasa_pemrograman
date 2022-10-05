c3b893df23e8a998cea613d77b321cd0400a83b4
root <root@JE.localdomain>
2022-10-05 21:00:42 +0700

    peretemuan_3

---
+++ pertemuan_3/type_data.py
@@ -0,0 +1,16 @@
+# type data di python
+
+#type data integer
+alva=12
+print (alva)
+print (type(alva))
+
+#type data float
+bepa=12.5
+print (bepa)
+print (type(bepa))
+
+#type data complex
+bahasa=12j
+print(bahasa)
+print(type(bahasa))
