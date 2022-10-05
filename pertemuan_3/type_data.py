32f8b172db6b072d6e16c582d7987e42afd06929
root <root@JE.localdomain>
2022-10-05 21:29:58 +0700

    jenis_type_data

--- pertemuan_3/type_data.py
+++ pertemuan_3/type_data.py
@@ -14,3 +14,39 @@ print (type(bepa))
 bahasa=12j
 print(bahasa)
 print(type(bahasa))
+
+#type data string
+A="hello world"
+print (A)
+print(type(A))
+
+#type data tuple
+B=("A","B","C")
+C=12,5,3,1
+print(B,C)
+print(type(B),type(C))
+
+#type data lis
+D=["A","B","C"]
+print(D)
+print(type(D))
+
+a=1
+a=2
+print(a)
+
+#type data set
+D={"saya", "sedang", "kuliah"}
+print(D)
+print(type(D))
+
+#type data dictonary
+D={"nama": "junius", "umur":20}
+print(D)
+print(type(D))
+
+#type data bool
+a=True
+b=False
+print(a,b)
+print(type(a),type(b))
