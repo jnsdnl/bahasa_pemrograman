04514762896d7d3767da160abd3785cc5e4623d6
root <root@JE.localdomain>
2022-10-11 18:48:43 +0700

    Tugas_sesi_3

--- pertemuan_3/tugas_sesi3.py
+++ pertemuan_3/tugas_sesi3.py
@@ -1,21 +1,93 @@
-a=20210801347
-b="junius daniel"
+#Contoh Operator Aritmatika
+a="operator aritmatika"
+d="operator aritmatika digunakan memecahkan kasus yang berhubungan dengan hitunh menghitung"
 
-c="sample operator in python"
 print(a)
+print(d)
+
+y, z = 20, 5
+
+penjumlahan = y+z #nilai1 ditambah nilai2
+print("Hasil dari y+z =", penjumlahan)
+
+#Contoh Operator perbandingan
+b= "operator perbandingan"
+a="operator perbandingan bertugas untuk membandingkan antar dua operan"
+
 print(b)
+print(a)
+
+y, z = 20, 5
+
+print("apakah y lebih besar dari z")
+print("y > z \t:", y>z) #Lebih besar
+
+#Contoh Operator Penugasan
+c= "Operator Penugasan"
+b="operator penugasan untuk memasukkan suatu nilai ke dalam variabel"
+
 print(c)
-nilai1=int(input("nilai 1 = "))
-nilai2=int(input("nilai 2 = "))
-penjumlahan=nilai1+nilai2
+print(b)
 
-pengurangan=nilai1-nilai2
+j = 5
+j += 3
 
-perkalian=nilai1*nilai2
+print("hasil dari penambahan dari input 1 ditambah dengan input 2 dengan variabel yang sama")
+print (j)
 
-pembagian=nilai1/nilai2
+#Contoh Operator Logika/Boolean
+d= "Operator Logika/Boolean"
+c="Operator Logika digunakan untuk membandingkan dua Operand atau dua nilai yang bertipe Boolean dan akan menghasilkan nilai Boolean yaitu TRUE atau FALSE"
 
-print("nilai penjumlahan ", penjumlahan)
-print("nilai pengurangan", pengurangan)
-print("nilai perkalian", perkalian)
-print("nilai pembagian", pembagian)
+print(d)
+print(c)
+
+y = True
+z = False
+l = y and z
+
+print("hasil dari logika AND")
+print ("True or False =", l)
+
+#Contoh Operator Keanggotaan
+q= "Operator Keanggotaan"
+w= "Fungsi dari operator ini adalah untuk memeriksa apakah suatu nilai merupakan salah satu anggota dari variabel berjenis sequence atau tidak"
+
+print(q)
+print(w)
+
+x = (2,5,8)
+y = 8
+
+print ("hasil dari operator in")
+print (8 in x)
+
+#Contoh Operator Identitas
+g= "Operator identitas"
+o= "Tugasnya adalah untuk mengetahui apakah dua buah variabel merupakan objek yang sama atau memiliki nilai yang sama atau tidak"
+
+print(g)
+print(o)
+
+h = 8
+k = h
+
+print ("hasil dari operator is")
+print (k is h)
+
+#Contoh Operator Bitwise
+l="Operator Bitwise"
+m="Jenis operator terakhir ini hampir sama seperti Operator Logika, akan tetapi operator ini melakukan operasi berdasarkan bilangan bit/biner"
+
+print(l)
+print(m)
+
+print("biner dari angka 0")
+print(format(0, '08b'))
+
+v= 1
+p= 22
+
+print("[and]")
+print('v & p =', v & p)
+print(format(v, '08b'), '&', format(p, '08b'), '=', format(v & p, '08b'), '\n')
