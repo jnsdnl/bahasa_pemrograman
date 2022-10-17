ac68ff24f43eca3aefc99621571e9855a4adf10c
root <root@JE.localdomain>
2022-10-17 12:16:57 +0700

    Catatan_Pertemuan_4

--- pertemuan_4/variabel.py
+++ pertemuan_4/variabel.py
@@ -1,13 +1,6 @@
-# VARIABEL
+#Variabel
+print("====Variabel====")
 
-# a=10
-# # print(a)
-
-
-# y, z = 20, 5
-
-# penjumlahan = y+z #nilai1 ditambah nilai2
-# print("Hasil dari y+z =", penjumlahan)
 
 def penjumlahan():
 
@@ -27,3 +20,108 @@ while True:
         break
     else:
         print("pilihan salah")
+
+
+#Fungsional
+print("====Fungsional====")
+
+def input_alas_dan_tinggi ():
+  alas = float(input('Masukkan alas: '))
+  tinggi = float((input('Masukkan tinggi: ')))
+
+  return alas, tinggi
+
+def hitung_luas (alas, tinggi):
+  return 0.5 * alas * tinggi
+
+"""kalau fungsional, kita sendiri yang mengelola
+hasil kembaliannya"""
+
+# satu fungsi bisa dipanggil secara independen
+#print(hitung_luas(5, 10))
+
+# contoh dengan inputan alas dan tinggi
+alas, tinggi = input_alas_dan_tinggi()
+print(hitung_luas(alas, tinggi))
+
+
+#Looping
+print("====Looping====")
+
+# looping python with range
+for i in range(5):
+    print(i)
+
+#looping python with break
+for i in range(1,11):
+    print(i,' x ',i ,' = ',i*i)
+    if i == 5:
+        break
+
+# looping python with continue
+for i in range(1,11):
+    if i == 5:
+        continue
+    print(i,' x ',i ,' = ',i*i)
+
+# nested loop
+listCity = ['Kalimantan', 'Sumatra', 'Sulawesi']
+listPlace = ['West', 'North', 'South']
+for city in listCity:
+    for place in listPlace:
+        print(city, place)
+
+
+
+# contoh penambahan terkait break/continue
+for val in "bahasa":
+    if val == "h":
+        continue
+        #break
+    print (val)
+print ("selesai")
+
+#OOP (Object Oriented Programing)
+print("====OOP====")
+
+class Segitiga:
+  def __init__(self, alas, tinggi):
+    self.alas = alas
+    self.tinggi = tinggi
+
+  def get_luas(self):
+    return 0.5 * self.alas * self.tinggi
+
+segitiga1 = Segitiga(5, 10)
+
+
+print('luas segitiga1:', segitiga1.get_luas())
+
+
+class Kotak:
+    def input1(self):
+        self.p = int(input("masukan panjang\t: "))
+        self.l = int(input("masukan lebar\t: "))
+
+    def luas(self):
+        self.luas1 = self.p*self.l
+
+kotak1 = Kotak()
+kotak1.input1()
+kotak1.luas()
+
+print ("luas kotak\t:",kotak1.luas1)
+
+
+#Prosedural
+print("====Prosedural====")
+
+def hitung_luas ():
+  alas = float(input('Masukkan alas: '))
+  tinggi = float(input('Masukkan tinggi: '))
+  print('Luas =', 0.5 * alas * tinggi)
+
+# kita bisa panggil berkali-kali
+hitung_luas()
+# panggil lagi
+# hitung_luas()
